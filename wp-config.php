b0ac437883590823b5adf82c11cd897a359892ef
Ratchada Jududom <ratchada.jududom@gmail.com>
2021-06-17 08:19:55 -0700

    wp-config

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,22 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', $_ENV[`DB_NAME`] );
+define( 'DB_NAME', getenv('DB_NAME') );
 
 /** MySQL database username */
-define( 'DB_USER', $_ENV[`DB_USER`] );
+define( 'DB_USER', getenv('DB_USER') );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', $_ENV[`DB_PASSWORD`] );
+define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
 
 /** MySQL hostname */
-define( 'DB_HOST', $_ENV[`DB_HOST`] );
+define( 'DB_HOST', getenv('DB_HOST') );
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', $_ENV[`DB_CHARSET`] );
+define( 'DB_CHARSET', getenv('DB_CHARSET') );
 
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', $_ENV[`DB_COLLATE`] );
+define( 'DB_COLLATE', getenv('DB_COLLATE') );
 
 /**#@+
  * Authentication Unique Keys and Salts.
