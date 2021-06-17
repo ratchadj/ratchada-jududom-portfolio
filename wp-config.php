79dfe17dc2259e70ef365e1a0ccb00483a61d62b
Ratchada Jududom <ratchada.jududom@gmail.com>
2021-06-17 11:25:48 -0700

    add upload_max_size

--- wp-config.php
+++ wp-config.php
@@ -101,3 +101,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+
+@ini_set( 'upload_max_size' , '20M' );
+@ini_set( 'post_max_size', '13M');
