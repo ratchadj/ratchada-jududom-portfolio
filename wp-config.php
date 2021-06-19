30267ee5a044006e7210bc9d8fbdfc0cd6498e87
Ratchada Jududom <ratchada.jududom@gmail.com>
2021-06-18 19:23:07 -0700

    remove error getenv_docker

--- wp-config.php
+++ wp-config.php
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         getenv_docker('WORDPRESS_AUTH_KEY',         'ba9a6825d78410cf36b18c7cf7e9edd7480a9f64') );
-define( 'SECURE_AUTH_KEY',  getenv_docker('WORDPRESS_SECURE_AUTH_KEY',  '737148dca986fd3b429d364839d7f5e545933a21') );
-define( 'LOGGED_IN_KEY',    getenv_docker('WORDPRESS_LOGGED_IN_KEY',    '6573e1f7e72f38371d4ea672da3ea222ec04eef1') );
-define( 'NONCE_KEY',        getenv_docker('WORDPRESS_NONCE_KEY',        'ec2a5b4cfa2d7e948ac39392d1bfabb78f7038f2') );
-define( 'AUTH_SALT',        getenv_docker('WORDPRESS_AUTH_SALT',        '7688228755499e1e3fc4c72dd80bbacd08bb041d') );
-define( 'SECURE_AUTH_SALT', getenv_docker('WORDPRESS_SECURE_AUTH_SALT', '754745a00531cecca67292c3aa16d365fe148cde') );
-define( 'LOGGED_IN_SALT',   getenv_docker('WORDPRESS_LOGGED_IN_SALT',   'c7d318aa0432cc3982c771db6fc2265ba11a9ea9') );
-define( 'NONCE_SALT',       getenv_docker('WORDPRESS_NONCE_SALT',       'eeb41827e42ce5cd258c58a9fd5ddcefdfa81601') );
+define( 'AUTH_KEY',         'ba9a6825d78410cf36b18c7cf7e9edd7480a9f64' );
+define( 'SECURE_AUTH_KEY',  '737148dca986fd3b429d364839d7f5e545933a21' );
+define( 'LOGGED_IN_KEY',    '6573e1f7e72f38371d4ea672da3ea222ec04eef1' );
+define( 'NONCE_KEY',        'ec2a5b4cfa2d7e948ac39392d1bfabb78f7038f2' );
+define( 'AUTH_SALT',        '7688228755499e1e3fc4c72dd80bbacd08bb041d' );
+define( 'SECURE_AUTH_SALT', '754745a00531cecca67292c3aa16d365fe148cde' );
+define( 'LOGGED_IN_SALT',   'c7d318aa0432cc3982c771db6fc2265ba11a9ea9' );
+define( 'NONCE_SALT',       'eeb41827e42ce5cd258c58a9fd5ddcefdfa81601' );
 
 /**#@-*/
 
