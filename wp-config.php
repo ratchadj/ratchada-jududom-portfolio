0ceb818f2d9b79392bbf516457b1d1ec94610659
Ratchada Jududom <ratchada.jududom@gmail.com>
2021-06-19 10:51:44 -0700

    disable debug

--- wp-config.php
+++ wp-config.php
@@ -77,7 +77,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', true );
+define( 'WP_DEBUG', false );
 
 // Disable display of errors and warnings
 define( 'WP_DEBUG_DISPLAY', true );
